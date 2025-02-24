809b41ce5e084222f5d052a08dd725541931f61c
Roxy Light <roxy@256lights.llc>
2025-02-24 10:36:04 -0800

    Add copyright headers to files

--- cmd/mustache-codegen/js_test.go
+++ cmd/mustache-codegen/js_test.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 Kagi Search
+// SPDX-License-Identifier: MIT
+
 package main
 
 import (
--- go/mustache/mustache_test.go
+++ go/mustache/mustache_test.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2025 Kagi Search
+// SPDX-License-Identifier: MIT
+
 package mustache
 
 import (
